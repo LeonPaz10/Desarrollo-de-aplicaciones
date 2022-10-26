d07ba1b07146d07fc180e2b8e3635c932eeb7c84
Leon Paz <82189243+LeonPaz10@users.noreply.github.com>
2022-10-26 20:06:44 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.letra} >Hola coder! esta es mi primer aplicacion</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  letra:{
+    color:'green'
+  }
+});
