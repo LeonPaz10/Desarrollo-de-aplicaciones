58f9b23a59b10f351653734ea52bbfc1babeb6ff
Leon Paz <82189243+LeonPaz10@users.noreply.github.com>
2022-11-09 18:27:22 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+
+import Modal from "./components/Modal";
 
 export default function App() {
+  const [textItem, setTextItem] = useState("");
+  const [list, setList] = useState([]);
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [itemSelected, setItemSelected] = useState({});
+
+  const onHandleChange = (t) => setTextItem(t);
+
+  const addItem = () => {
+    setList((currentState) => [
+      ...currentState,
+      { id: Math.random().toString(), value: textItem },
+    ]); 
+    setTextItem("");
+   
+  };
+
+  const selectedItem = (id) => {
+    console.log(id);
+    setItemSelected(list.find((item) => item.id === id));
+    setModalVisible(true);
+  };
+
+  const deleteItem = () => {
+    console.log(itemSelected);
+    setList((currentState) =>
+      currentState.filter((item) => item.id !== itemSelected.id)
+    );
+    setItemSelected({});
+    setModalVisible(false);
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => selectedItem(item.id)}>
+      <Text>{item.value}</Text>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.letra} >Hola coder! esta es mi primer aplicacion</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 32, color:"#000" }}>Lista de compras 🛒</Text>
+      <View style={styles.inputcontainer}>
+        <TextInput
+          placeholder="Agregar producto"
+          placeholderTextColor="white"
+          style={styles.inputStyle}
+          value={textItem}
+          onChangeText={onHandleChange}
+        />
+        <TouchableOpacity style={styles.button} onPress={addItem}>
+          <Text> Agregar </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <FlatList
+          data={list}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}          
+        />
+      </View>
+      <Modal isVisible={modalVisible} actionDeleteItem={deleteItem} />
     </View>
   );
 }
@@ -13,11 +80,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#EE9654",
+    alignItems: "center",
+    paddingTop: 100,
   },
-  letra:{
-    color:'green'
-  }
+  inputcontainer: {
+    marginTop: 40,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "100%",
+    paddingHorizontal: 30,
+    margin:50
+  },
+  inputStyle: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    width: 200,
+    padding: 5,
+    marginBottom:10
+  },
+  button: {
+    backgroundColor: "#D0B472",
+    height: 35,
+    width: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+  },
+
 });
